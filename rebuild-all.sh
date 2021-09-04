a85d5d9014b15cd2defb4604663bfc45f903fce1
Emil Johansson <emil.sweden@gmail.com>
2021-09-04 17:38:40 +0200

    Update python instead of golang.

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,7 +1,7 @@
 podman pull registry.fedoraproject.org/fedora-toolbox:34
 podman build -t emacs-base emacs-base
-podman build -t golang golang
+podman build -t python python
 podman rm base
-podman rm go
+#podman rm python
 toolbox create -i localhost/emacs-base base
-toolbox create -i localhost/golang go
+toolbox create -i localhost/python python
