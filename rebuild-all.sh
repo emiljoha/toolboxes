bf41983bea545f069074956857b37e907b0fc011
Emil Johansson <emil.sweden@gmail.com>
2021-09-06 19:49:23 +0200

    Organize, organize, organize.

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,7 +1,11 @@
 podman pull registry.fedoraproject.org/fedora-toolbox:34
-podman build -t emacs-base emacs-base
-podman build -t python python
+
+podman build -t base base
+podman stop base
 podman rm base
-#podman rm python
-toolbox create -i localhost/emacs-base base
+toolbox create -i localhost/base base
+
+podman build -t python python
+podman stop python
+podman rm python
 toolbox create -i localhost/python python
