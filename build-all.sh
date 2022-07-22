9995428d9e70e1417c68940d39d6c7447743c3be
Emil Johansson <emil.sweden@gmail.com>
2022-07-22 17:18:25 +0200

    Update to fedora 36

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
 set -x
-podman pull registry.fedoraproject.org/fedora-toolbox:34
+podman pull registry.fedoraproject.org/fedora-toolbox:36
 bash build.sh base
 bash build.sh python
